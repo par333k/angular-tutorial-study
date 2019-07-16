4aced3e2ca55d2769819477cf2927a7652e3694e
par333k <par333k@naver.com>
2019-07-16 14:12:03 +0900

    2 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
@@ -18,7 +18,13 @@ export class CartService {
     this.items = [];
     return this.items;
   }
+
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
+  }
   
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
 
 }
